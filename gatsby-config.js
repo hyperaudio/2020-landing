9e8cf28114c808479e3aeb6d3d191e9b40ac5deb
Laurian Gridinoc <laurian@gridinoc.name>
2021-02-02 17:38:08 +0200

    disable csp

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,23 +69,23 @@ module.exports = {
         extensions: [],
       },
     },
-    {
-      resolve: `gatsby-plugin-csp`,
-      options: {
-        disableOnDev: true,
-        reportOnly: false,
-        mergeScriptHashes: true,
-        mergeStyleHashes: false,
-        mergeDefaultDirectives: true,
-        directives: {
-          'script-src': "'self'",
-          'style-src': "'self' 'unsafe-inline' fonts.googleapis.com",
-          'font-src': "'self' fonts.gstatic.com",
-          'img-src': "'self' data: www.google-analytics.com",
-          'frame-ancestors': "'none'",
-          'form-action': "'none'",
-        },
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-csp`,
+    //   options: {
+    //     disableOnDev: true,
+    //     reportOnly: false,
+    //     mergeScriptHashes: true,
+    //     mergeStyleHashes: false,
+    //     mergeDefaultDirectives: true,
+    //     directives: {
+    //       'script-src': "'self'",
+    //       'style-src': "'self' 'unsafe-inline' fonts.googleapis.com",
+    //       'font-src': "'self' fonts.gstatic.com",
+    //       'img-src': "'self' data: www.google-analytics.com",
+    //       'frame-ancestors': "'none'",
+    //       'form-action': "'none'",
+    //     },
+    //   },
+    // },
   ],
 };
