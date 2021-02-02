8b21f6f485f72e8cc66bba2e7e36c9e4d181b302
Laurian Gridinoc <laurian@gridinoc.name>
2021-02-02 17:15:25 +0200

    more CSP for google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,8 +79,8 @@ module.exports = {
         mergeDefaultDirectives: true,
         directives: {
           'script-src': "'self'",
-          'style-src': "'self' https://fonts.googleapis.com",
-          'font-src': "'self'  https://fonts.googleapis.com",
+          'style-src': "'self' fonts.googleapis.com",
+          'font-src': "'self'  fonts.gstatic.com",
           'img-src': "'self' data: www.google-analytics.com",
           'frame-ancestors': "'none'",
           'form-action': "'none'",
